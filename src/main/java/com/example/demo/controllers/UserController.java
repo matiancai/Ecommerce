52a8c05911df67a3c29127de8ad4fa8637e7b515
matiancai74 <matthew.chermside@mail.com>
2019-12-30 10:56:33 +0800

    Logging added to UserController class

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -5,14 +5,20 @@ import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired private UserRepository userRepository;
 	
@@ -22,15 +28,28 @@ public class UserController {
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<AppUser> findById(@PathVariable Long id) {
-		return ResponseEntity.of(userRepository.findById(id));
+		log.info("Searching for user with Id: " + id);
+		Optional<AppUser> appUser = userRepository.findById(id);
+		if(appUser.get()!=null){
+			log.info("User retrieved with Id: " + id);
+			return ResponseEntity.ok(appUser.get());
+		}else{
+			log.error("Id not found: " + id);
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	@GetMapping("/{username}")
 	public ResponseEntity<AppUser> findByUserName(@PathVariable String username) {
-		System.err.println(username);
+		log.info("Searching for user with username: " + username);
 		AppUser appUser = userRepository.findByUsername(username);
-
-		return appUser == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(appUser);
+		if(appUser!=null){
+			log.info("User retrieved with username: " + username);
+			return ResponseEntity.ok(appUser);
+		}else{
+			log.error("Username not found: " + username);
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	@PostMapping("/create")
@@ -43,12 +62,13 @@ public class UserController {
 
 		if(createUserRequest.getPassword().length() < 7 ||
 			!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
-			System.err.println("Error with appUser password. Cannot Create appUser: " + createUserRequest.getUsername());
+			log.error("Error with appUser password. Cannot Create appUser: " + createUserRequest.getUsername());
 			return ResponseEntity.badRequest().build();
 		}
 
 		appUser.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(appUser);
+		log.info("New user created: " + createUserRequest.getUsername());
 		System.err.println(appUser);
 		return ResponseEntity.ok(appUser);
 	}
