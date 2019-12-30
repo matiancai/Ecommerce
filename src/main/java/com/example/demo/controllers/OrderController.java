c66b22c73a4ab7582db3291a6bd7401b35138f51
matiancai74 <matthew.chermside@mail.com>
2019-12-30 15:34:14 +0800

    Controllers now log data to file tracked by Splunk

--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -7,6 +7,7 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
@@ -14,29 +15,36 @@ import java.util.List;
 @RequestMapping("/orders")
 public class OrderController {
 
+	private static final Logger log = Logger.getLogger(OrderController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private OrderRepository orderRepository;
 
-	@PostMapping("/submit/{username}")
-	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
-		AppUser appUser = userRepository.findByUsername(username);
+	@PostMapping("/submit/{userName}")
+	public ResponseEntity<UserOrder> submit(@PathVariable String userName) {
+		AppUser appUser = userRepository.findByUsername(userName);
 		if(appUser == null) {
+			log.info("OrderSubmit = failure username = " + userName);
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(appUser.getCart());
 		orderRepository.save(order);
+		log.info("OrderSubmit = success username = " + userName);
 		return ResponseEntity.ok(order);
 	}
 	
-	@GetMapping("/history/{username}")
-	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
-		AppUser appUser = userRepository.findByUsername(username);
+	@GetMapping("/history/{userName}")
+	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String userName) {
+		log.info("OrderHistorySearch =" + userName);
+		AppUser appUser = userRepository.findByUsername(userName);
 		if(appUser == null) {
+			log.info("OrderHistorySearchResult = failure username = " + userName);
 			return ResponseEntity.notFound().build();
 		}
+		log.info("OrderHistorySearchResult = success username = " + userName);
 		return ResponseEntity.ok(orderRepository.findByAppUser(appUser));
 	}
 }
